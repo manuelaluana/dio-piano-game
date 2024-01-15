00151c1eda5e8886bfbcc6e65f829d5500113450
ManuelaLuanaSchumackerTavares <manuelaluana.dev@gmail.com>
2024-01-15 16:02:17 -0300

    Adiciona efeito de click a opcao de utilizar as teclas do teclado para tocar

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -5,14 +5,21 @@ let audio = new Audio("./src/tunes/a.wav");
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
   audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 pianoKeys.forEach((key) => {
-  console.log(key.dataset.key);
+  //   console.log(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 document.addEventListener("keydown", (e) => {
   playTune(e.key);
-  console.log(e.key);
+  //   console.log(e.key);
 });
